aedee925492da6c67d760b9a523c1763b58dbb8a
jenizeth-chess <jenizethlucii@gmail.com>
2025-03-30 23:05:51 -0600

    13 hash passwords

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require("bcrypt");
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 
 //END_ASYNC
