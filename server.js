a8087c2521d2784114a63d647c4485fdf013d90b
jenizeth-chess <jenizethlucii@gmail.com>
2025-04-01 21:59:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 const bcrypt = require("bcrypt");
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+//13. Hash and Compare Passwords Asynchronously
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
@@ -23,7 +23,7 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
-
+//14. Hash and Compare Passwords Synchronously
 var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
 var result = bcrypt.compareSync(myPlaintextPassword, hash);
 console.log(hash);
